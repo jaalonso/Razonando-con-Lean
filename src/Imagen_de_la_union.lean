bd9da5fde51ee11300e89f6f7918c0a4f25f970b
Jose A. Alonso <JoseA.Alonso@gmail.com>
2022-05-04 14:33:56 +0200

    Imagen_de_la_union

--- src/Imagen_de_la_union.lean
+++ src/Imagen_de_la_union.lean
@@ -1,7 +1,7 @@
 -- Imagen_de_la_union.lean
 -- Imagen de la unión.
 -- José A. Alonso Jiménez
--- Sevilla, 8 de junio de 2021
+-- Sevilla, 4 de mayo de 2022
 -- ---------------------------------------------------------------------
 
 -- ---------------------------------------------------------------------
@@ -22,12 +22,6 @@ variables {α : Type*} {β : Type*}
 variable  f : α → β
 variables s t : set α
 
-#check mem_image 
-#check mem_image_of_mem
-#check mem_union
-#check mem_union_left
-#check mem_union_right
-
 -- 1ª demostración
 -- ===============
 
@@ -39,32 +33,36 @@ begin
     rw mem_image at h,
     cases h with x hx,
     cases hx with xst fxy,
-    rw ← fxy,
-    rw mem_union,
-    cases xst with xs xt,
-    { left,
+    rw ← fxy, 
+    rw mem_union at xst,
+    cases xst with xs xt, 
+    { apply mem_union_left,
       apply mem_image_of_mem,
       exact xs, },
-    { right,
+    { apply mem_union_right,
       apply mem_image_of_mem,
       exact xt, }},
   { intro h,
     rw mem_union at h,
     cases h with yfs yft,
-    { rw mem_image at yfs,
+    { rw mem_image,
+      rw mem_image at yfs,
       cases yfs with x hx,
       cases hx with xs fxy,
-      rw ← fxy,
-      apply mem_image_of_mem,
-      apply mem_union_left,
-      exact xs, },
-    { rw mem_image at yft,
+      use x,
+      split,
+      { apply mem_union_left,
+        exact xs, },
+      { exact fxy, }},
+    { rw mem_image,
+      rw mem_image at yft,
       cases yft with x hx,
       cases hx with xt fxy,
-      rw ← fxy,
-      apply mem_image_of_mem,
-      apply mem_union_right,
-      exact xt, }},
+      use x,
+      split,
+      { apply mem_union_right,
+        exact xt, },
+      { exact fxy, }}},
 end
 
 -- 2ª demostración
@@ -74,21 +72,18 @@ example : f '' (s ∪ t) = f '' s ∪ f '' t :=
 begin
   ext y,
   split,
-  { rintro ⟨x, xst, fxy⟩,
-    rw ← fxy,
-    cases xst with xs xt,
+  { rintro ⟨x, xst, rfl⟩,
+    cases xst with xs xt, 
     { left,
       exact mem_image_of_mem f xs, },
     { right,
       exact mem_image_of_mem f xt, }},
-  { rintros (yfs | yft),
-    { rcases yfs with ⟨x, xs, fxy⟩,
-      rw ← fxy,
+  { rintro (yfs | yft),
+    { rcases yfs with ⟨x, xs, rfl⟩,
       apply mem_image_of_mem,
       left,
       exact xs, },
-    { rcases yft with ⟨x, xt, fxy⟩,
-      rw ← fxy,
+    { rcases yft with ⟨x, xt, rfl⟩,
       apply mem_image_of_mem,
       right,
       exact xt, }},
@@ -102,20 +97,16 @@ begin
   ext y,
   split,
   { rintro ⟨x, xst, rfl⟩,
-    cases xst with xs xt,
+    cases xst with xs xt, 
     { left,
-      exact mem_image_of_mem f xs, },
+      use [x, xs], },
     { right,
-      exact mem_image_of_mem f xt, }},
-  { rintros (yfs | yft),
+      use [x, xt], }},
+  { rintro (yfs | yft),
     { rcases yfs with ⟨x, xs, rfl⟩,
-      apply mem_image_of_mem,
-      left,
-      exact xs, },
+      use [x, or.inl xs], },
     { rcases yft with ⟨x, xt, rfl⟩,
-      apply mem_image_of_mem,
-      right,
-      exact xt, }},
+      use [x, or.inr xt], }},
 end
 
 -- 4ª demostración
@@ -125,52 +116,17 @@ example : f '' (s ∪ t) = f '' s ∪ f '' t :=
 begin
   ext y,
   split,
-  { rintro ⟨x, xst, rfl⟩,
-    cases xst with xs xt,
+  { rintro ⟨x, xs | xt, rfl⟩,
     { left,
       use [x, xs], },
     { right,
       use [x, xt], }},
-  { rintros (yfs | yft),
-    { rcases yfs with ⟨x, xs, rfl⟩,
-      use [x, or.inl xs], },
-    { rcases yft with ⟨x, xt, rfl⟩,
-      use [x, or.inr xt], }},
+  { rintros (⟨x, xs, rfl⟩ | ⟨x, xt, rfl⟩),
+    { use [x, or.inl xs], },
+    { use [x, or.inr xt], }},
 end
 
 -- 5ª demostración
--- ===============
-
-example : f '' (s ∪ t) = f '' s ∪ f '' t :=
-begin
-  ext y,
-  split,
-  { rintros ⟨x, xs | xt, rfl⟩,
-    { left,
-      use [x, xs] },
-    { right,
-      use [x, xt] }},
-  { rintros (⟨x, xs, rfl⟩ | ⟨x, xt, rfl⟩),
-    { use [x, or.inl xs] },
-    { use [x, or.inr xt] }},
-end
-
--- 6ª demostración
--- ===============
-
-example : f '' (s ∪ t) = f '' s ∪ f '' t :=
-begin
-  ext y,
-  split,
-  { rintros ⟨x, xs | xt, rfl⟩,
-    { finish, },
-    { finish, }},
-  { rintros (⟨x, xs, rfl⟩ | ⟨x, xt, rfl⟩),
-    { finish, },
-    { finish, }},
-end
-
--- 7ª demostración
 -- ===============
 
 example : f '' (s ∪ t) = f '' s ∪ f '' t :=
@@ -181,7 +137,7 @@ begin
   { rintros (⟨x, xs, rfl⟩ | ⟨x, xt, rfl⟩) ; finish, },
 end
 
--- 8ª demostración
+-- 6ª demostración
 -- ===============
 
 example : f '' (s ∪ t) = f '' s ∪ f '' t :=
@@ -192,25 +148,24 @@ begin
   { finish, },
 end
 
--- 9ª demostración
+-- 7ª demostración
 -- ===============
 
 example : f '' (s ∪ t) = f '' s ∪ f '' t :=
 begin
   ext y,
   rw iff_def,
-  finish,
+  finish, 
 end
 
--- 10ª demostración
+-- 8ª demostración
 -- ===============
 
 example : f '' (s ∪ t) = f '' s ∪ f '' t :=
 by finish [ext_iff, iff_def]
 
--- 11ª demostración
+-- 9ª demostración
 -- ===============
 
 example : f '' (s ∪ t) = f '' s ∪ f '' t :=
--- by library_search
 image_union f s t
